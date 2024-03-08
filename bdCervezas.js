39f377107cc8236dedc4032140ede32aefc96926
martinmontesjavier <120597788+martinmontesjavier@users.noreply.github.com>
2024-03-08 18:28:30 +0100

    Add files via upload

---
+++ bdCervezas.js
@@ -0,0 +1,43 @@
+export const bd = [
+  {
+    id: 3,
+    nombre: "Mahou Cinco Estrellas",
+    tipo: "Lager",
+    origen: "Madrid",
+    descripcion: "Cerveza rubia, suave y refrescante con un sabor ligeramente amargo.",
+    imagen: "https://www.loscervecistas.es/wp-content/uploads/2021/10/mahou-cinco-estrellas-213770.png"
+  },
+  {
+    id: 22,
+    nombre: "Estrella Galicia",
+    tipo: "Lager",
+    origen: "Galicia",
+    descripcion: "Cerveza suave y equilibrada con un sabor ligeramente amargo y aroma a malta.",
+    imagen: "https://www.crusat.com/wp-content/uploads/2021/07/estrella-galicia-especial.png"
+  },
+  {
+    id: 33,
+    nombre: "Alhambra Reserva 1925",
+    tipo: "Lager",
+    origen: "Granada",
+    descripcion: "Cerveza rubia con un sabor ligeramente dulce y toques de caramelo.",
+    imagen: "https://sgfm.elcorteingles.es/SGFM/dctm/MEDIA03/202204/04/00118602800916____3__600x600.jpg"
+  },
+  {
+    id: 34,
+    nombre: "San Miguel Especial",
+    tipo: "Lager",
+    origen: "Barcelona",
+    descripcion: "Cerveza rubia, suave y refrescante con un sabor ligeramente amargo.",
+    imagen: "https://www.sanmiguel.com/es/wp-content/uploads/sites/2/2021/01/san-miguel-gluten-free-4.png"
+  },
+  {
+    id: 35,
+    nombre: "Damm Estrella",
+    tipo: "Lager",
+    origen: "Barcelona",
+    descripcion: "Cerveza rubia, suave y refrescante con un sabor ligeramente amargo.",
+    imagen: "https://static.damm.com/sites/default/files/config-page/estrella_header_logo/estrella-damm_0.png"
+  }
+];
+
